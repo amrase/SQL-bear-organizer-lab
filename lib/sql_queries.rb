7f71c634a3995486ac9f936dd6c6a40674e7b2d8
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-17 16:52:21 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,9 +19,9 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-    "select color,count(*) from bears where color='dark brown'"
+    "select color,count(*) from bears group by color order by count(*) desc limit 1 "
 end
-
+ 
 def counts_number_of_bears_with_goofy_temperaments
   "select count(*) from bears where temperament='goofy'"
 end
